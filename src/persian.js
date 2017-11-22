974a79b6b4a2fed4a6269b903b47f20565a30fb8
mahmoud <mahmood.sa225@gmail.com>
2017-11-22 15:16:59 +0330

    Add toEnglish Method
    A new method to convert persian numbers to english numbers

--- src/persian.js
+++ src/persian.js
@@ -49,6 +49,22 @@ function replaceEnglishToPersian(str) {
   return str.replaceAll(charMap);
 }
 
+function replacePersianToEnglish(str) {
+  const charMap = {
+    '۰': '0',
+    '۱': '1',
+    '۲': '2',
+    '۳': '3',
+    '۴': '4',
+    '۵': '5',
+    '۶': '6',
+    '۷': '7',
+    '۸': '8',
+    '۹': '9',
+  };
+  return str.replaceAll(charMap);
+}
+
 function toPersian(str, {
   arabic = true,
   english = true,
@@ -63,6 +79,11 @@ function toPersian(str, {
   return result;
 }
 
+function toEnglish(str) {
+  return replacePersianToEnglish(str);
+}
+
 module.exports = {
   toPersian,
+  toEnglish,
 };
